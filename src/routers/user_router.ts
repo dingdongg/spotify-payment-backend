dcfd7f0ea82fed713072aa9f2329395b25330bd4
dingdongg <cs.dg.kim@gmail.com>
2022-12-28 19:15:49 -0700

    create router for user operations

---
+++ src/routers/user_router.ts
@@ -0,0 +1,49 @@
+import express from 'express';
+import { IUser } from "../database/models/User";
+import UserController from '../controllers/UserController';
+
+const router = express();
+
+router.get("/:userId", async (req, res) => {
+    const userController = new UserController();
+
+    // need to include request/parmeter sanitizing to prevent SQL injections
+    try {
+        const user = await userController.getUser(req.params.userId);
+        res.status(200);
+        res.send(user);
+    } catch (error) {
+        res.status(500);
+        res.send("User could not be found.");
+    }
+});
+
+router.post("/register_user", async (req, res) => {
+    const userController = new UserController();
+
+    try {
+        await userController.createUser(req.body as IUser);
+        res.status(204).end();
+    } catch (error) {
+        res.status(500);
+        res.send("User could not be created.");
+    }
+});
+
+router.delete("/delete_user/:userId", async (req, res) => {
+    const userController = new UserController();
+
+    try {
+        await userController.deleteUser(req.params.userId);
+        res.status(204).end();
+    } catch (error) {
+        res.status(500);
+        res.send("User could not be deleted.");
+    }
+});
+
+router.put("/update_user/:userId", async (req, res) => {
+    // ... TODO
+});
+
+export default router;
