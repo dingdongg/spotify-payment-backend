1435d73480b8ef80dfe08a96b4e8f3862830e29e
Michael Tandyo <mtand920@mtroyal.ca>
2023-01-18 16:13:57 -0700

    some tests

--- tests/unit/user/payment.test.ts
+++ tests/unit/user/payment.test.ts
@@ -53,7 +53,7 @@ describe ("Payment Tests", ()=>{
             paymentDate: date,
             paymentStatus: Status[0].toString()
         };
-        console.log(paymentInfo)
+        
         
         await paymentController.createPayment({ 
             ...paymentInfo
@@ -71,8 +71,72 @@ describe ("Payment Tests", ()=>{
         
         
 })  
-    it ("PaymentController::createPayment successfully upload user in DB", async () =>{
+    it ("PaymentController::creation of payment history", async () =>{
+       //creating two different users
+       await Payments.deleteMany({});
+        const userInfo1 = {
+            firstName: "mahkel",
+            lastName: "test",
+            email: "michaeltest@gmail.com",
+            password: "test"
+        };
+        await userController.createUser({
+            ...userInfo1
+        })
+        // const userInfo2 = {
+        //     firstName: "donggyu",
+        //     lastName: "test",
+        //     email: "donggyutest@gmail.com",
+        //     password: "test"
+        // };
+        // await userController.createUser({
+        //     ...userInfo2
+        // })
 
+        // const userInstance = await User.find({}).exec();
+       
+
+        //create multiple payment for one guy
+        //lets start with mahkels
+        let userId;
+        try {
+            const user = await User.findOne({
+                email: "michaeltest@gmail.com"
+            }).exec();
+            
+            if (user) {
+                userId = user._id;
+            } else {
+                console.log("No user found with that email");
+            }
+        } catch (err) {
+            console.error(err);
+        }
+        
+        for (let i = 0; i < 3 ; i++){
+            
+            const paymentInfo = {
+                memberId: userId?.valueOf(),
+                paymentAmount: Math.floor(Math.random() * (69-3)+3),
+                paymentDate: new Date(),
+                paymentStatus: Status[Math.floor(Math.random()*(3-0))].toString()
+            };
+            await paymentController.createPayment({ 
+                ...paymentInfo
+            })
+        }
+        const paymentInstance = await Payments.find({});
+        console.log (paymentInstance)
+
+        // console.log(userId?.valueOf())
+        
+        let getAdminHistory = await paymentController.findUserPaymentHistory(userId?.valueOf());
+        console.log("test",getAdminHistory)
+        
+        
+        
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                  
+        
     })
 
 })
