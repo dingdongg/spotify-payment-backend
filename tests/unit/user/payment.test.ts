d9700fe740b89b2f90510905457e738786d77247
Michael <michaeltandyo@gmail.com>
2023-01-23 06:18:28 -0700

    theres a slight bug

--- tests/unit/user/payment.test.ts
+++ tests/unit/user/payment.test.ts
@@ -7,21 +7,24 @@ import UserController from "../../../src/controllers/UserController";
 import { User } from "../../../src/database/models/User";
 import Status from "../../../src/enum/Status"
 
+
 const expect = chai.expect;
+
 
 describe ("Payment Tests", ()=>{
     let paymentController: PaymentController;
     let userController : UserController;
-
+ 
     before (async ()=> {
         await connect();
-        await Payments.deleteMany({});
+        await Payments.deleteMany({}); 
         paymentController = new PaymentController();
         userController = new UserController();
     });
 
     it ("PaymentController::createPayment successfully upload user in DB", async () =>{
         //make mock user first
+        await Payments.deleteMany({});
         const userInfo = {
             firstName: "mahkel",
             lastName: "test",
@@ -71,7 +74,7 @@ describe ("Payment Tests", ()=>{
         
         
 })  
-    it ("PaymentController::creation of payment history", async () =>{
+    it ("PaymentController::findUserPaymentHistory finds user and sort them in order by date", async () =>{
        //creating two different users
        await Payments.deleteMany({});
         const userInfo1 = {
@@ -90,15 +93,15 @@ describe ("Payment Tests", ()=>{
         //     password: "test"
         // };
         // await userController.createUser({
+            
         //     ...userInfo2
         // })
 
         // const userInstance = await User.find({}).exec();
        
-
         //create multiple payment for one guy
         //lets start with mahkels
-        let userId;
+        let userId: Object|undefined;
         try {
             const user = await User.findOne({
                 email: "michaeltest@gmail.com"
@@ -125,18 +128,66 @@ describe ("Payment Tests", ()=>{
                 ...paymentInfo
             })
         }
-        const paymentInstance = await Payments.find({});
-        console.log (paymentInstance)
-
-        // console.log(userId?.valueOf())
+     
+        let getUserHistory = await paymentController.findUserPaymentHistory(userId?.valueOf());
         
-        let getAdminHistory = await paymentController.findUserPaymentHistory(userId?.valueOf());
-        console.log("test",getAdminHistory)
+        const paymentsArray = Object.values(getUserHistory);
         
+        const sortedPayments = paymentsArray.sort((a, b) => a.paymentDate.getTime() - b.paymentDate.getTime());
         
-        
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                  
+        expect(paymentsArray).to.deep.equal(sortedPayments);
+        expect(paymentsArray.every(payment => {return payment.memberId === userId?.valueOf() })).to.be.true                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                     
         
     })
 
+    it ("PaymentController:: getPaymentHistory", async () =>{
+        await User.deleteMany({});
+        const users = [
+            {firstName: "mahkel", lastName: "test", email: "mahkel1@example.com", password: "testpassword"},
+            {firstName: "donggyu", lastName: "test", email: "donggyu1@example.com", password: "testpassword"},
+            {firstName: "michael", lastName: "test", email: "michael1@example.com", password: "testpassword"}
+        ];
+        
+        
+        const createUsers = users.map(user => userController.createUser(user));
+        await Promise.all(createUsers);
+
+        const userInstance = await User.find({}).exec();
+       
+        let createPayments = userInstance.map(async(data)=>{
+            
+            let memberId = data._id?.valueOf();
+            
+            let payments = []
+            for (let i = 0; i < 3 ; i++){
+                console.log(memberId)
+                const paymentInfo = {
+                    memberId: memberId,
+                    paymentAmount: Math.floor(Math.random() * (69-3)+3),
+                    paymentDate: new Date(),
+                    paymentStatus: Status[Math.floor(Math.random()*(3-0))].toString()
+                };
+                //await paymentController.createPayment(paymentInfo);
+                payments.push(paymentController.createPayment(paymentInfo));
+                
+            }
+                return Promise.all(payments)
+            // await Promise.all(user)
+        })
+        await Promise.all(createPayments);
+
+        let getPaymentHistory = await paymentController.getPaymentHistory()
+        console.log(getPaymentHistory)
+
+        const paymentsArray = Object.values(getPaymentHistory);
+        const sortedPayments = paymentsArray.sort((a, b) => a.paymentDate.getTime() - b.paymentDate.getTime());
+
+        expect(paymentsArray).to.deep.equal(sortedPayments);
+        //expect(paymentsArray.length).to.equal(9);
+
+        //test findUserPaymentHistory multiple 
+
+        //console.log(paymentsArray[0]._id)
+    })
+ 
 })
