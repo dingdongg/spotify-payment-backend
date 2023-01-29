cdd48c5b15975e1c1811f246c07837a119fb88b9
dingdongg <cs.dg.kim@gmail.com>
2023-01-29 00:04:07 -0800

    add CSRF token to SessionData type

--- types/express-session/index.d.ts
+++ types/express-session/index.d.ts
@@ -1,7 +1,9 @@
+import { CsrfSyncedToken } from "csrf-sync";
 import "express-session";
 
 declare module "express-session" { // additional typing to support userId
     export interface SessionData {
         userId: string;
+        csrfToken: CsrfSyncedToken;
     }
 }
