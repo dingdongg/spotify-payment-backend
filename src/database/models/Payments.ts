9960e4f1ebd4bac074ed7b2840114ca663b7e384
Michael <michaeltandyo@gmail.com>
2023-01-29 14:59:48 -0700

    finished pull request

--- src/database/models/Payments.ts
+++ src/database/models/Payments.ts
@@ -2,7 +2,7 @@ import { Schema, model } from "mongoose";
 
 export interface IPayment {
 
-    memberId: Object |undefined ,
+    memberId: String |undefined ,
     paymentAmount: Number,
     paymentDate:Date,
     paymentStatus: String;
@@ -10,7 +10,7 @@ export interface IPayment {
 }
 
 const paymentsSchema = new Schema <IPayment>({
-    memberId: { type: Object, required: true, ref: 'User'},
+    memberId: { type: String , required: true, ref: 'User'},
     paymentAmount: { type: Number, required: true },
     paymentDate: { type: Date, required: true },
     paymentStatus: { type: String, required: true }
