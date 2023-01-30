0cc15837bbe03580e60d58eb92e6947d25930f02
dingdongg <cs.dg.kim@gmail.com>
2023-01-29 18:19:10 -0800

    change param types

--- src/controllers/PaymentController.ts
+++ src/controllers/PaymentController.ts
@@ -12,11 +12,11 @@ export default class PaymentController {
         }
     }
 
-    public async deletePayment(paymentId: string | undefined): Promise<void> {
+    public async deletePayment(paymentId: string): Promise<void> {
         await Payments.findByIdAndDelete(paymentId).exec();
     }
 
-    public async editPayment(paymentId: string | undefined, newInfo: IPayment): Promise<void> {
+    public async editPayment(paymentId: string, newInfo: IPayment): Promise<void> {
         await Payments.findByIdAndUpdate(paymentId, newInfo).exec();
     }
     
@@ -25,7 +25,7 @@ export default class PaymentController {
         
     }
 
-    public async findUserPaymentHistory(userId: string|undefined): Promise<Object>{
+    public async findUserPaymentHistory(userId: string): Promise<Object>{
        
         return await Payments.find({memberId: userId}).sort({paymentDate: 1}).exec();
         
