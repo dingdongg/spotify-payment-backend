1e751d43e7bb84854539832abb4e0c50dd772647
Michael <michaeltandyo@gmail.com>
2023-02-08 00:09:04 -0700

    finished email services

--- src/services/EmailServices.ts
+++ src/services/EmailServices.ts
@@ -7,9 +7,14 @@ const DOMAIN = process.env.MAILGUN_DOMAIN as string
 
 let mailgun = mailgunLoader.default ({apiKey: apiKey, domain: DOMAIN});
 
-function sendEmail (to:string, from:string, subject:string, content:string){
-    let data = {to,from,subject,text:content};
+const sendEmail =(to:string, from:string, subject:string, content:string) => {
+    let data = {
+        to,
+        from,
+        subject,
+        text:content
+    };
     //sends a promise
     return mailgun.messages().send(data);
-}
+};
 
