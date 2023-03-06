229520abd89c2bc0135bd68d7fa73534dff69ab0
Michael <michaeltandyo@gmail.com>
2023-03-05 20:43:48 -0700

    commit

--- src/services/EmailServices.ts
+++ src/services/EmailServices.ts
@@ -4,10 +4,11 @@ import express from 'express';
 import dotenv from "dotenv";
 dotenv.config();
 const apiKey = process.env.MAILGUN_APIKEY as string
+const publicApiKey = process.env.MAILGUN_PUBLIC as string
 const DOMAIN = process.env.MAILGUN_DOMAIN as string
 
-let mailgun = mailgunLoader.default ({apiKey: apiKey, domain: DOMAIN});
-
+const mailgun = mailgunLoader.default ({apiKey: apiKey, domain: DOMAIN});
+const publicMailgun = mailgunLoader.default ({apiKey: publicApiKey, domain: DOMAIN});
 const router = express()
 
 const sendEmail =(to:string, from:string, subject:string, content:string) => {
@@ -29,6 +30,24 @@ router.post("/send-email", async(req,res)=>{
         console.log(e)
         res.status(500);
     }
+})
+
+router.post("/verify-email", async (req,res)=>{
+    const userEmail = req.body.email
+    console.log(userEmail)
+    if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(userEmail)){
+       try{
+        const validationEmail = await publicMailgun.validate(userEmail);
+        console.log(validationEmail)
+       } catch (error){
+        console.log(error)
+       }
+    
+    return (true)
+  }
+    alert("You have entered an invalid email address!")
+    return (false)
+
 })
 
 export default router;
